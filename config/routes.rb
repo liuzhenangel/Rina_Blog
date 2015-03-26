d3902c636d0cebd50d8ad44087cb5b06994b0329
liuzhenangel <550686383@qq.com>
2015-03-27 00:37:15 +0800

    优化代码

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,23 @@
 Rails.application.routes.draw do
-  get 'photo/create'
 
   namespace :admin do
     resources :posts
   end
+
   resources :articles
   resources :comments
   post '/preview', to: 'admin/posts#preview'
   post '/photos', to: 'admin/posts#photos'
-  root 'welcome#index'
-  get '/about', to: 'welcome#about'
-  get '/blog/rss', to: 'welcome#rss'
-  get '/all', to: 'articles#index'
+
   post '/search', to: 'articles#search'
   get  '/search', to: 'articles#search'
+
   post '/signin', to: 'admin/sessions#create'
   get '/signin', to: 'admin/sessions#new'
   get '/signout', to: 'admin/sessions#destroy'
+
+  get '/blog/rss', to: 'welcome#rss'
+  get '/about', to: 'welcome#about'
+  get '/all', to: 'articles#index'
+  root 'welcome#index'
 end
