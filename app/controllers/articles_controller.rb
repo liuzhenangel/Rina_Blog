8daeba199eeac8cf9bed9762b5003327b9f135e5
liuzhenangel <550686383@qq.com>
2015-03-27 00:09:09 +0800

    重构搜索功能

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -10,15 +10,12 @@ class ArticlesController < ApplicationController
   end
 
   def search
-    @newest_articles = Article.where("title like ? OR content like ?", "%#{params[:search]}%", "%#{params[:search]}%")
-    if params[:newest]
-      @newest = Article.find(params[:newest])
-      @comments = @newest.comments.order(created_at: :desc)
-      render 'welcome/index'
-    else
-      @article = Article.find(params[:article])
-      @comments = @article.comments.order(created_at: :desc)
-      redirect_to @article
+    @articles = Article.where("title like ? OR content like ?", "%#{params[:search]}%", "%#{params[:search]}%")
+    @articles.map do |a|
+      a.title = a.title.gsub(params[:search], "<em>#{params[:search]}</em>")
+      unless a.content.include?('/uploads/photo/image')
+        a.content = a.content.gsub(params[:search], "<em>#{params[:search]}</em>")
+      end
     end
   end
 end
