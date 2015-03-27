8331a98c5298f1fbdb303dc6d921c174288c532f
liuzhenangel <550686383@qq.com>
2015-03-27 15:20:44 +0800

    设置上传图片大小

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -4,7 +4,7 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
-  # include CarrierWave::MiniMagick
+  include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
   storage :file
@@ -32,6 +32,9 @@ class ImageUploader < CarrierWave::Uploader::Base
   # end
 
   # Create different versions of your uploaded files:
+
+  process :resize_to_fit => [747, 500]
+
   # version :thumb do
   #   process :resize_to_fit => [50, 50]
   # end
