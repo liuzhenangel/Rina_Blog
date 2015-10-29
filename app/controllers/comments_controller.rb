bc25e71c667efdf4fa938a3a1752e61b46151608
liuzhenangel <550686383@qq.com>
2015-10-29 12:37:51 +0800

    屏蔽评论

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,15 +4,15 @@ class CommentsController < ApplicationController
 
   def create
     @article = Article.find(params[:article_id]).to_html
-    @comment = Comment.new(comment_params.merge(article_id: params[:article_id]))
-    @comments = @article.comments.order(created_at: :desc)
-    if !@comment.save
-      flash[:error] = '添加评论失败'
-      render 'articles/show'
-    else
+    #@comment = Comment.new(comment_params.merge(article_id: params[:article_id]))
+    #@comments = @article.comments.order(created_at: :desc)
+    #if !@comment.save
+    #  flash[:error] = '添加评论失败'
+    #  render 'articles/show'
+    #else
       flash[:notic] = '添加评论成功'
       redirect_to @article
-    end
+   # end
   end
 
   private
